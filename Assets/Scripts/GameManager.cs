980c50fd196f63f734bcafbb69c14533e74e9687
tyredd <treddin@student.fitchburgstate.edu>
2023-12-12 12:47:15 -0500

    Randomized Track Created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,16 +8,20 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject titleScreen;
+    public List<GameObject> tiles;
+    public GameObject tile;
     public TextMeshProUGUI victoryScreen;
     public TextMeshProUGUI failureScreen;
     public Button restartButton;
     public bool isGameActive = false;
+    private float tileWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-
+        tileWidth = tile.GetComponent<MeshRenderer>().bounds.size.z;
+        Shuffle(tiles);
+        PlaceTiles(tiles);
     }
 
     // Update is called once per frame
@@ -31,6 +35,27 @@ public class GameManager : MonoBehaviour
         isGameActive = !isGameActive;
         Debug.Log("Boolean value is now: " + isGameActive);
         titleScreen.gameObject.SetActive(false);
+    }
+
+    void Shuffle(List<GameObject> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            GameObject temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+
+    void PlaceTiles(List<GameObject> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            // Arrange objects in a horizontal line
+            list[i].transform.position = new Vector3(0, 0, (i + 1) * tileWidth);
+            Instantiate(list[i], transform);
+        }
     }
 
     public void RestartGame()
